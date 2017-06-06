46337a65455c46d44d21866f7b84fbb9c9956d6b
Idan Lottan <idan2110@gmail.com>
2017-06-06 18:28:23 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://idanlo:yelpcamp@ds111922.mlab.com:11922/idanloyelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
